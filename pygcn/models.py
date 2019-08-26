c1d2c738939214020a9c0982bee757febf339cb5
hoang dzung <dungmin97@gmail.com>
2019-08-27 02:30:49 +0700

    swap relu and softmax order

--- pygcn/models.py
+++ pygcn/models.py
@@ -12,8 +12,9 @@ class GCN(nn.Module):
         self.dropout = dropout
 
     def forward(self, x, adj, temp=1, hard=False, beta=0):
-        x = F.relu(self.gc1(x, adj))
+        x = self.gc1(x, adj)
         embedding_assign = gumbel_softmax(x, temp, hard, beta)
+        x = F.relu(x)
         x = F.dropout(x, self.dropout, training=self.training)
         x = self.gc2(x, adj)
         return embedding_assign, F.log_softmax(x, dim=1)
