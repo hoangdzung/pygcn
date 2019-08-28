991a883c10a3db72df47d3ac7fe483219a57602e
hoang dzung <dungmin97@gmail.com>
2019-08-28 15:17:47 +0700

    add node2vec loss

--- pygcn/train_n2v.py
+++ pygcn/train_n2v.py
@@ -9,9 +9,9 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-from pygcn.utils import load_data, accuracy
+from pygcn.utils import load_data, accuracy, fixed_unigram_candidate_sampler
 from pygcn.models import GCN
-from pygcn.loss import nmin_cut
+from pygcn.loss import nmin_cut, node2vec
 from pygcn.classify import classify
 
 # Training settings
@@ -20,11 +20,15 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
                     help='Disables CUDA training.')
 parser.add_argument('--fastmode', action='store_true', default=False,
                     help='Validate during training pass.')
-parser.add_argument('--seed', type=int, default=42, help='Random seed.')
+parser.add_argument('--seed', type=int, default=42, 
+                    help='Random seed.')
+
 parser.add_argument('--pre_epochs', type=int, default=200,
                     help='Number of epochs to pre-train.')
 parser.add_argument('--epochs', type=int, default=200,
                     help='Number of epochs to train.')
+parser.add_argument('--batch_size', type=int, default=256,
+                    help='Batch size.')
 parser.add_argument('--lr', type=float, default=0.01,
                     help='Initial learning rate.')
 parser.add_argument('--weight_decay', type=float, default=5e-4,
@@ -33,6 +37,10 @@ parser.add_argument('--hidden', type=int, default=16,
                     help='Number of hidden units.')
 parser.add_argument('--dropout', type=float, default=0.5,
                     help='Dropout rate (1 - keep probability).')
+parser.add_argument('--neg_sample_size',  type=int, default=20,
+                    help='Negative sample size')
+parser.add_argument('--neg_sample_weight',  type=int, default=20,
+                    help='Negative sample size')
 
 parser.add_argument('--anneal', action='store_true', default=False,
                     help='Disables CUDA training.')
@@ -56,7 +64,7 @@ if args.cuda:
     torch.cuda.manual_seed(args.seed)
 
 # Load data
-adj, adj_ds, _, _, features, labels, idx_train, idx_val, idx_test = load_data()
+adj, adj_ds, train_edges, degrees, features, labels, idx_train, idx_val, idx_test = load_data()
 
 # Model and optimizer
 model = GCN(nfeat=features.shape[1],
@@ -93,8 +101,23 @@ def pretrain(epoch, temp):
     t = time.time()
     model.train()
     optimizer.zero_grad()
-    assign_tensor, output = model(features, adj, temp, args.hard, args.beta)
-    loss_train = nmin_cut(assign_tensor, adj_ds) 
+    _, _ = model(features, adj, temp, args.hard, args.beta)
+    neg_nodes = fixed_unigram_candidate_sampler(
+        num_sampled=args.neg_sample_size,
+        unique=False,
+        range_max=len(degrees),
+        distortion=0.75,
+        unigrams=degrees
+    )   
+    np.random.shuffle(train_edges)
+    batch_edges = train_edges[:args.batch_size]
+    nodes1, nodes2 = batch_edges[:,0], batch_edges[:,1]
+
+    outputs1 = F.normalize(model.params[nodes1], dim=1)
+    outputs2 = F.normalize(model.params[nodes2], dim=1)
+    neg_outputs = F.normalize(model.params[neg_nodes], dim=1)
+
+    loss_train = node2vec(outputs1, outputs2, neg_outputs, args.neg_sample_weight)
 #     acc_train = accuracy(output[idx_train], labels[idx_train])
     loss_train.backward()
     optimizer.step()
